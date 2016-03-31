ec4eb99a02991d6cd56937dc9aff003e2da1ed01
Rodrigo Soria <rodsilver83@hotmail.com>
2016-03-31 12:42:11 -0600

    passport config

--- server.js
+++ server.js
@@ -4,10 +4,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose'),
-  express = require('./config/express');
-var db = mongoose();
+  express = require('./config/express'),
+  passport = require('./config/passport');
 
+var db = mongoose();
 var app = express();
+var passport = passport();
+
 app.listen(3000);
 module.exports = app;
 
