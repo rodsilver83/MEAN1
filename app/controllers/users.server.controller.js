c2ccb415dfb82ec61f98a0cc066d2c464272c2d3
Rodrigo Soria <rodsilver83@hotmail.com>
2016-07-06 14:42:49 -0500

    CRUD express mongoose

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -160,3 +160,12 @@ exports.saveOAuthUserProfile = function(req, profile, done) {
   });
 };
 
+exports.requiresLogin = function(req,res, next){
+  if( !req.isAuthenticated()){
+    return res.status(401).send({
+      message: 'User is not logged on'
+    });
+  }
+  next();
+}
+
