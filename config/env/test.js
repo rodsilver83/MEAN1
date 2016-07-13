adad644c48eecaf30b672cca9f420a6d88421139
Rodrigo Soria <rodsilver83@hotmail.com>
2016-07-13 17:09:05 -0500

    Express Testing

---
+++ config/env/test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Rodrigo on 2/17/16.
+ */
+module.exports = {
+  //Development configuration options
+  db: 'mongodb://localhost/mean-book-test',
+  sessionSecret: 'testSessionSecret',
+  viewEngine: 'ejs',
+  facebook: {
+    clientID: '1200062096726267',
+    clientSecret: '7a05e6c1adbc41df44dd10888c0eeffd',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
+};
--- public/articles/services/articles.client.service.js
+++ public/articles/services/articles.client.service.js
@@ -2,8 +2,7 @@
  * Created by Rodrigo on 7/6/16.
  */
 angular.module('articles')
-.factory('Articles', ['$resource',
-function($resource){
+  .factory('Articles', ['$resource', function($resource){
   return $resource('api/articles/:articleId', {
     articleId: '@_id'
   },{
